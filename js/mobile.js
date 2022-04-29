6a5f385cf33717813452422f71953bd6d33c80ba
godwinc77 <godwinoviec77@gmail.com>
2022-04-29 14:04:59 -0900

    mobile update

--- js/mobile.js
+++ js/mobile.js
@@ -1,2 +1,33 @@
 const bars = document.getElementsByClassName("fa-bars")[0]
 const lastList = document.getElementById("navListLast")
+const check = document.getElementsByClassName("look")[0]
+const cancel = document.getElementsByClassName("delete")[0]
+const undo = document.getElementsByClassName("erase")[0]
+const test = document.getElementsByClassName("find")[0]
+function switchBars(){
+    lastList.style.display="block"
+    bars.style.visibility="hidden"
+    cancel.style.visibility="visible"
+    closeBar()
+}
+function cancleBar(){
+    lastList.style.display="none"
+    bars.style.visibility="visible"
+    cancel.style.visibility="hidden"
+}
+function searchBar(){
+    test.style.display="block"
+    check.style.visibility="hidden"
+    undo.style.visibility="visible"
+    cancleBar()
+}
+function closeBar(){
+    test.style.display="none"
+    check.style.visibility="visible"
+    undo.style.visibility="hidden"
+    
+}
+bars.addEventListener("click",switchBars)
+cancel.addEventListener("click",cancleBar)
+check.addEventListener("click",searchBar)
+undo.addEventListener("click",closeBar)
